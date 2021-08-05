c4fa22a059c190afb047107874268732a7ffccd3
Tushar Mehta <56684401+Tushar00728@users.noreply.github.com>
2021-08-06 04:46:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, request, jsonify
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.externals import joblib
+import joblib
 
 filename = "model.pkl"
 clf = pickle.load(open(filename, 'rb'))
